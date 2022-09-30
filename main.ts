b317542d36334f3c113c2106e646df5d6cc64d4b
nswobodaSHA <113388294+nswobodaSHA@users.noreply.github.com>
2022-09-30 14:23:35 -0700

    working on explosion

--- main.ts
+++ main.ts
@@ -6,11 +6,41 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
     fireball.setFlag(SpriteFlag.Invisible, false)
+    rocketflag = 1
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
+    if (Math.abs(apple.vx) > 20 || apple.vy > 50) {
+        explosion = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . 2 2 . . 2 2 . 2 . . . . . 
+            . . . 2 2 2 . 2 2 . 2 2 . . . . 
+            . . . . 2 2 2 . 2 2 2 2 2 . . . 
+            . . 2 2 . . 2 2 2 2 2 2 . . . 2 
+            . . 2 2 2 2 2 4 4 4 4 2 . 2 2 2 
+            . . . 2 2 2 4 4 4 4 4 2 2 2 2 . 
+            . . 2 2 2 4 4 4 4 4 4 4 4 2 2 . 
+            . 2 2 4 4 5 4 4 4 5 4 5 4 2 2 . 
+            . 2 2 4 4 4 4 4 5 5 4 4 4 4 2 2 
+            2 2 2 4 4 4 4 4 5 5 4 4 4 4 2 2 
+            2 2 4 4 4 4 5 5 5 5 5 5 4 4 2 2 
+            2 4 4 4 4 5 5 5 4 5 5 5 5 4 4 2 
+            2 4 4 4 5 5 5 5 5 5 5 5 5 4 4 2 
+            2 4 4 4 5 5 5 5 5 5 5 5 5 4 4 2 
+            `, SpriteKind.Player)
+        explosion.setPosition(apple.x, apple.y)
+        scene.cameraFollowSprite(explosion)
+        pause(1000)
+        game.over(false)
+    } else {
+        if (landingflag == 0) {
+            fuel += 500
+            landingflag = 1
+        }
+    }
     apple.setVelocity(0, -1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -19,10 +49,18 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
     fireball.setFlag(SpriteFlag.Invisible, true)
+    rocketflag = 0
 })
+let explosion: Sprite = null
 let apple: Sprite = null
 let angle = 0
 let fireball: Sprite = null
+let rocketflag = 0
+let landingflag = 0
+landingflag = 0
+let fuel = 10000
+let fuelsprite = textsprite.create(convertToText(fuel))
+rocketflag = 0
 fireball = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -73,11 +111,20 @@ apple.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
+    fuelsprite.destroy()
+    fuelsprite = textsprite.create(convertToText(fuel))
     minimap2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
     minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
     minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
-    minimap2.setPosition(apple.x - 50, apple.y - 30)
+    minimap2.setPosition(apple.x - 50, apple.y - 20)
     engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
     fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    fuelsprite.setPosition(apple.x - 50, apple.y - 50)
+    if (rocketflag == 1) {
+        fuel += -10
+    }
+    if (apple.y < 150) {
+        landingflag = 0
+    }
 })
